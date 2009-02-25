9ce4be892fba9e094dbeb0bb9e352f94bb4a9263
Nana <nana@seed2.(none)>
2009-02-25 11:04:11 -0700

    Fixed Sofa, instead of stupidly not declaring the package. Also included it. Haven't written tests for it yet because I'm a backwards, dumb motherfucker. Will write basic run tests now I guess.

--- lib/Idiom.pm
+++ lib/Idiom.pm
@@ -4,14 +4,21 @@ use strict;
 use warnings;
 
 use base 'Mojo';
+use Idiom::Sofa;
+
+my $cdb = Idiom::Sofa->new( host => 'localhost', port => 5984 );
+my $peopledb = $cdb->db("people");
 
 sub handler {
     my ($self, $tx) = @_;
 
     # $tx is a Mojo::Transaction instance
     $tx->res->code(200);
-    $tx->res->headers->content_type('text/plain');
-    $tx->res->body('Hello Mojo!');
+    $tx->res->headers->content_type('text/html');
+    # $tx->res->body('Hello Mojo!');
+
+    my $bodyc = "<html><body><h1>hello.</h1></body></html>\n\n";
+    $tx->res->body($bodyc);
 
     return $tx;
 }
--- lib/Idiom/Sofa.pm
+++ lib/Idiom/Sofa.pm
@@ -1,4 +1,5 @@
 #!/usr/bin/perl
+package Idiom::Sofa;
 use base 'Net::CouchDb';
 
 1;
